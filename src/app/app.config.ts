30d3d937b7412284015bf32494829090edc367b7
Medelkouch <m.elkouch@elham.sa>
2023-11-29 16:48:35 +0100

    initial global styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { RouterModule, provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { StoreModule, provideStore } from '@ngrx/store';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    importProvidersFrom([
+      BrowserModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      RouterModule.forRoot(routes),
+      ToastrModule.forRoot({ preventDuplicates: true }),
+    ]),
+  ],
 };
