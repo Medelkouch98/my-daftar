22a9af87a5ef1c85aea278085dbcefb47de507e9
Medelkouch <m.elkouch@elham.sa>
2023-11-29 22:04:10 +0100

    feat:login page

--- src/app/features/authentication/authentication/authentication.component.ts
+++ src/app/features/authentication/authentication/authentication.component.ts
@@ -1,11 +1,10 @@
-import { MatButtonModule } from '@angular/material/button';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink],
   templateUrl: './authentication.component.html',
 })
 export class AuthenticationComponent {}
--- src/app/features/authentication/login/login.component.ts
+++ src/app/features/authentication/login/login.component.ts
@@ -1,9 +1,12 @@
+import { MatInputModule } from '@angular/material/input';
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, RouterLink],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {}
