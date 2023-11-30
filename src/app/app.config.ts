566118052db6bb92816bcb78bac169fb821d2f5a
Medelkouch <m.elkouch@elham.sa>
2023-11-30 01:04:50 +0100

    feat:signup wip

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { RouterModule, provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { StoreModule, provideStore } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -15,12 +15,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideStore(),
     importProvidersFrom([
-      BrowserModule,
-      BrowserAnimationsModule,
-      CommonModule,
-      HttpClientModule,
-      RouterModule.forRoot(routes),
-      ToastrModule.forRoot({ preventDuplicates: true }),
+        BrowserModule,
+        BrowserAnimationsModule,
+        CommonModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes),
+        ToastrModule.forRoot({ preventDuplicates: true }),
     ]),
-  ],
+    provideAnimations()
+],
 };
--- src/app/features/authentication/signup/signup.component.ts
+++ src/app/features/authentication/signup/signup.component.ts
@@ -1,9 +1,67 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    MatStepperModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './signup.component.html',
+  styles: [
+    `
+      ::ng-deep .mat-step-icon-selected {
+        background-color: #005212 !important;
+      }
+      ::ng-deep .mat-step-icon-state-edit {
+        background-color: #005212 !important;
+      }
+      ::ng-deep .mdc-notched-outline {
+        border: 2px solid #005212;
+        border-radius: 8px;
+        margin-top: 5px;
+        left: -6px !important;
+        height: 48px !important;
+        width: 70vw !important;
+        max-width: 100vw !important;
+      }
+    `,
+  ],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  firstFormGroup = this._formBuilder.group({
+    email: [''],
+    username: [''],
+    password: [''],
+    confirmPassword: [''],
+  });
+  secondFormGroup = this._formBuilder.group({
+    lastName: [''],
+    firstName: [''],
+    city: [''],
+    birthDate: [''],
+  });
+  isOptional = false;
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
